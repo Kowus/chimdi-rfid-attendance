deb3cbd0f1405509b7942f83f9806871be7cba4c
kowus <barnabas@elite-education.org>
2017-09-06 04:58:15 +0000

    mailgun

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var api_key = process.env.MAILGUN_API_KEY;
-var domain = 'www.mydomain.com';
+var domain = process.env.MAILGUN_DOMAIN;
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 const passport = require('passport');
