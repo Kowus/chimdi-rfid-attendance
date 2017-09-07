23dc478b681383ff104222f4231bf2b21c021876
kowus <barnabas@elite-education.org>
2017-09-07 03:57:43 +0000

    added attendance to user models

--- routes/api.js
+++ routes/api.js
@@ -96,7 +96,7 @@ router.get('/signin/card', (req, res, next) => {
                             "student_fname":user.firstname,
                             "student_lname":user.lastname,
                             "student_index":user.indexNumber,
-                            "student_present":true
+                            "student_present":req.query.present || false
                         }],
                             $position: 0
                     }
